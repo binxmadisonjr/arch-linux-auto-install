f0b0564095bb62925c13a5dd4daf9815de98197a
k ostaN <thenintiescalled@gmail.com>
2024-10-18 14:47:33 -0500

    Update arch_install.sh
    Improved installation script with visible output and added os-prober result display.

--- arch_install.sh
+++ arch_install.sh
@@ -9,41 +9,40 @@ TIMEZONE="America/Chicago"
 LOCALE="en_US.UTF-8"
 
 # Update system clock
-timedatectl set-ntp true > /dev/null
+timedatectl set-ntp true
 
 # Partition formatting
 echo "Formatting partitions..."
-mkfs.fat -F32 $BOOT_PARTITION > /dev/null
-mkfs.ext4 -F $ROOT_PARTITION > /dev/null # Force format the root partition
-mkswap $SWAP_PARTITION > /dev/null
+mkfs.fat -F32 $BOOT_PARTITION
+mkfs.ext4 -F $ROOT_PARTITION # Force format the root partition
+mkswap $SWAP_PARTITION
 
 # Mount partitions
 echo "Mounting partitions..."
-mount $ROOT_PARTITION /mnt > /dev/null
-mkdir /mnt/boot > /dev/null
-mount $BOOT_PARTITION /mnt/boot > /dev/null
-swapon $SWAP_PARTITION > /dev/null
+mount $ROOT_PARTITION /mnt
+mkdir /mnt/boot
+mount $BOOT_PARTITION /mnt/boot
+swapon $SWAP_PARTITION
 
 # Install base system
 echo "Installing base system..."
-pacstrap /mnt base linux linux-firmware nano iwd dhcpcd netctl > /dev/null
+pacstrap /mnt base linux linux-firmware nano iwd dhcpcd netctl
 
 # Generate fstab
 echo "Generating fstab..."
-genfstab -U /mnt >> /mnt/etc/fstab > /dev/null
+genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot into the new system
-arch-chroot /mnt > /dev/null <<EOF
+arch-chroot /mnt <<EOF
 
 # Set time zone
 echo "Setting time zone..."
-ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime > /dev/null
-hwclock --systohc > /dev/null
+ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+hwclock --systohc
 
 # Set locale
-echo "Setting locale..."
-echo "$LOCALE UTF-8" > /etc/locale.gen
-locale-gen > /dev/null
+echo "$LOCALE" > /etc/locale.gen
+locale-gen
 echo "LANG=$LOCALE" > /etc/locale.conf
 
 # Set hostname
@@ -52,28 +51,29 @@ echo "$HOSTNAME" > /etc/hostname
 
 # Enable iwd for wireless management
 echo "Enabling iwd..."
-systemctl enable iwd > /dev/null
-systemctl start iwd > /dev/null
+systemctl enable iwd
+systemctl start iwd
 
 # Enable DHCP for network management
 echo "Enabling DHCP..."
-systemctl enable dhcpcd > /dev/null
-systemctl start dhcpcd > /dev/null
+systemctl enable dhcpcd
+systemctl start dhcpcd
 
 # Install and configure bootloader (GRUB)
 echo "Installing GRUB..."
-pacman -S grub efibootmgr os-prober ntfs-3g --noconfirm > /dev/null
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB > /dev/null
+pacman -S grub efibootmgr os-prober ntfs-3g --noconfirm
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 
 # Enable OS Prober in GRUB
-sed -i 's/GRUB_DISABLE_OS_PROBER=true/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub > /dev/null
+sed -i 's/GRUB_DISABLE_OS_PROBER=true/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
 
-# Manually run os-prober
-os-prober > /dev/null 
+# Manually run os-prober and print results
+echo "os-prober output:"
+os-prober 
 
 # Generate GRUB config
 echo "Generating GRUB configuration..."
-grub-mkconfig -o /boot/grub/grub.cfg > /dev/null
+grub-mkconfig -o /boot/grub/grub.cfg
 
 # Set root password
 echo "Setting root password..."
@@ -83,5 +83,5 @@ EOF
 
 # Unmount partitions and reboot
 echo "Finishing installation..."
-umount -R /mnt > /dev/null
-shutdown now > /dev/null
+umount -R /mnt
+shutdown now
