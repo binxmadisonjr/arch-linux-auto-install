339478f4dd57683f56526c5f6a289fe779db8930
k ostaN <thenintiescalled@gmail.com>
2024-10-18 15:28:03 -0500

    Added git and base-devel to the package installation
    - Added git and base-devel packages to ensure development tools and version control are available right after installation.
    - Ensured the EFI partition is mounted properly for GRUB configuration.
    - Made sure that network connectivity is enabled through iwd and DHCP.

--- arch_install.sh
+++ arch_install.sh
@@ -4,6 +4,7 @@
 BOOT_PARTITION="/dev/nvme0n1p5"
 ROOT_PARTITION="/dev/nvme0n1p6"
 SWAP_PARTITION="/dev/nvme0n1p7"
+EFI_PARTITION="/dev/nvme0n1p1"
 HOSTNAME="archlinux"
 TIMEZONE="America/Chicago"
 LOCALE="en_US.UTF-8"
@@ -24,9 +25,14 @@ mkdir /mnt/boot
 mount $BOOT_PARTITION /mnt/boot
 swapon $SWAP_PARTITION
 
+# Mount the EFI partition for GRUB setup
+echo "Mounting EFI partition..."
+mkdir /mnt/boot/efi
+mount $EFI_PARTITION /mnt/boot/efi
+
 # Install base system
 echo "Installing base system..."
-pacstrap /mnt base linux linux-firmware nano iwd dhcpcd netctl
+pacstrap /mnt base linux linux-firmware nano iwd dhcpcd netctl git base-devel
 
 # Generate fstab
 echo "Generating fstab..."
@@ -59,10 +65,15 @@ echo "Enabling DHCP..."
 systemctl enable dhcpcd
 systemctl start dhcpcd
 
+# Set up DNS in /etc/resolv.conf for name resolution
+echo "Setting up DNS..."
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+
 # Install and configure bootloader (GRUB)
 echo "Installing GRUB..."
 pacman -S grub efibootmgr os-prober ntfs-3g --noconfirm
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 
 # Enable OS Prober in GRUB
 sed -i 's/GRUB_DISABLE_OS_PROBER=true/GRUB_DISABLE_OS_PROBER=false/' /etc/default/grub
